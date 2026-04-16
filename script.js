45506a95020bcae7af65ae37f69b048b29a125d6
praveenkumar91939 <praveen91939@gmail.com>
2026-04-16 11:00:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Fade-in animation on scroll
+const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+});
